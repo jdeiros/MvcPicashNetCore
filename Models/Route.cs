77589367cdfedc342eb7379c7fcc8addd5e986a4
Jerónimo Deiros <jeronimodeiros@gmail.com>
2019-04-25 11:16:03 -0300

    route model

--- Models/Route.cs
+++ Models/Route.cs
@@ -7,9 +7,11 @@ namespace MvcPicashNetCore.Models
     public class Route
     {
         public string RouteId { get; set; }
-        [Display(Prompt ="Código de Ruta", Name = "Código de Ruta")]
+        [Display(Prompt ="Cod. Ruta", Name = "Cod. Ruta")]
         public string Code { get; set; }
+        [Display(Prompt ="Nombre Ruta", Name = "Nombre Ruta")]
         public string Name { get; set; }
+        [Display(Prompt ="Cobrador", Name = "Cobrador")]
         public string DebtCollectorId { get; set; }
         public DebtCollector DebtCollector { get; set; }
         public List<Customer> Customers { get; set; }
